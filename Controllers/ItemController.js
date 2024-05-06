073c3096fccf023de106c68950e4df72f413408e
shraman <pkpaulpostdn@gmail.com>
2024-05-06 07:36:28 +0530

    added modules for delete

--- Controllers/ItemController.js
+++ Controllers/ItemController.js
@@ -4,14 +4,15 @@ const Watchlist = require('../Models/Watchlist');
 module.exports.addToWatchList = async (req, resp) => {
     try {
         const { asset_name, asset_id } = req.body;
-        // const username = req.cookies.username;
+        const username = req.cookies.username;
+        
         console.log("asset id", asset_id);
         console.log("asset name", asset_name);
         const asset_info = AssetLink.links[asset_id];
-
+        console.log(username);
         let watchlist = await Watchlist.findOne({asset_id});
         if (!watchlist) {
-            console.log("username", username);
+            // console.log("username", username);
             console.log("asset info", asset_info);
             const newWatchlistItem = new Watchlist({ username, asset_id, asset_name, asset_info });
             const insertResult = await newWatchlistItem.save();
@@ -20,6 +21,35 @@ module.exports.addToWatchList = async (req, resp) => {
             return { message: 'User does not exist or Data already exists' };
         }
     } catch (err) {
+        console.error(err);
+        resp.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports.getWatchList = async (req, resp) => {
+    try {
+        const username = req.cookies.username;
+        console.log(username);
+        const watchlist = await Watchlist.find({username});
+        resp.status(200).json({ watchlist });
+    }
+    catch (err) {
+        console.error(err);
+        resp.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports.deleteWatchList = async (req, resp) => {
+    try {
+        const { asset_id } = req.body;
+        const username = req.cookies.username;
+        console.log(username);
+        console.log(asset_id);
+        const watchlist = await Watchlist.findOneAndDelete({username, asset_id});
+        console.log(watchlist);
+        resp.status(200).json({ message: 'Deleted from watchlist' });
+    }
+    catch (err) {
         console.error(err);
         resp.status(500).json({ message: 'Internal Server Error' });
     }
--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -3,5 +3,7 @@ const router = Router();
 const ItemController = require('../Controllers/ItemController')
 
 router.post('/watchlist',ItemController.addToWatchList);
+router.get('/getwatchlist',ItemController.getWatchList);
+router.delete('/deletewatchlist',ItemController.deleteWatchList);
 
 module.exports = router;
