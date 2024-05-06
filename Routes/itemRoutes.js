77e85cac5c8ce978543d60bd3747084842697475
Zaid <predcodes695@gmail.com>
2024-05-06 11:17:23 +0530

    add: proteced watchlist route

--- Routes/itemRoutes.js
+++ Routes/itemRoutes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express')
 const router = Router();
-const ItemController = require('../Controllers/ItemController')
+const ItemController = require('../Controllers/ItemController');
+const { verifyUser } = require('../MiddleWares/Auth');
 
-router.post('/watchlist',ItemController.addToWatchList);
-router.get('/getwatchlist',ItemController.getWatchList);
-router.delete('/deletewatchlist',ItemController.deleteWatchList);
+router.post('/watchlist', verifyUser, ItemController.addToWatchList);
+router.get('/getwatchlist',verifyUser, ItemController.getWatchList);
+router.delete('/deletewatchlist', verifyUser, ItemController.deleteWatchList);
 
 module.exports = router;
